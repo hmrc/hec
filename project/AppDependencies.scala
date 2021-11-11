9dd79e58803f6bdb6482900d4b6270a1ef9db4fe
priyankadhiman3006 <88194246+priyankadhiman3006@users.noreply.github.com>
2021-11-11 15:44:01 +0000

    HEC-1398 hmrc mongo lib for test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,10 +5,11 @@ object AppDependencies {
 
   val akkaVersion      = "2.6.14"
   val bootstrapVersion = "5.16.0"
+  val hmrcMongoVersion = "0.56.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"   % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"          % "0.56.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"          % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "domain"                      % "6.2.0-play-28",
     "ai.x"                    %% "play-json-extensions"        % "0.42.0",
     "org.typelevel"           %% "cats-core"                   % "2.6.1",
@@ -19,14 +20,14 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion    % Test,
-    "uk.gov.hmrc"            %% "reactivemongo-test"     % "5.0.0-play-28"     % Test,
-    "org.scalatest"          %% "scalatest"              % "3.2.10"            % Test,
-    "com.typesafe.play"      %% "play-test"              % PlayVersion.current % Test,
-    "org.scalamock"          %% "scalamock"              % "5.1.0"             % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"           % "0.62.2"            % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"             % "test, it",
-    "com.typesafe.akka"      %% "akka-testkit"           % akkaVersion         % Test,
-    "com.miguno.akka"        %% "akka-mock-scheduler"    % "0.5.1"             % Test
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVersion    % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-27" % hmrcMongoVersion    % Test,
+    "org.scalatest"          %% "scalatest"               % "3.2.10"            % Test,
+    "com.typesafe.play"      %% "play-test"               % PlayVersion.current % Test,
+    "org.scalamock"          %% "scalamock"               % "5.1.0"             % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"            % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"             % "test, it",
+    "com.typesafe.akka"      %% "akka-testkit"            % akkaVersion         % Test,
+    "com.miguno.akka"        %% "akka-mock-scheduler"     % "0.5.1"             % Test
   )
 }
