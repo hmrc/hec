560a7de07ea2b2c43a5170ebeb27a9da7c69be30
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2022-04-29 17:26:38 +0100

    HEC-2092: reset sent flags

---
+++ app/uk/gov/hmrc/hec/services/scheduleService/ResetTaxChecksSentFlagService.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hec.services.scheduleService
+
+import com.google.inject.{Inject, Singleton}
+import play.api.Configuration
+import uk.gov.hmrc.hec.repos.HECTaxCheckStore
+import uk.gov.hmrc.hec.services.MongoLockService
+import uk.gov.hmrc.hec.util.Logging
+import uk.gov.hmrc.hec.util.Logging._
+
+import java.time.ZonedDateTime
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+@Singleton
+class ResetTaxChecksSentFlagService @Inject() (
+  config: Configuration,
+  mongoLockService: MongoLockService,
+  taxCheckStore: HECTaxCheckStore
+)(implicit ex: HECTaxCheckExtractionContext)
+    extends Logging {
+
+  private val resetSentFlagsOnStart: Boolean =
+    config.get[Boolean]("hec-file-extraction-details.reset-sent-flags.enabled")
+
+  private val resetTaxChecksCreatedOnOrAfter: ZonedDateTime =
+    ZonedDateTime.parse(
+      config.get[String]("hec-file-extraction-details.reset-sent-flags.reset-tax-checks-created-on-or-after")
+    )
+
+  if (resetSentFlagsOnStart) {
+    resetSentFlags().onComplete {
+      case Failure(e)       => logger.warn(s"Could not run job reset 'sent' flags for tax check data: ${e.getMessage}")
+      case Success(None)    => logger.info("Could not get lock to reset 'sent' flag for tax check data")
+      case Success(Some(_)) => logger.info("Job to reset 'sent' flag for tax check data complete")
+    }
+  } else {
+    logger.info("Reset of 'sent' flag for tax check data not enabled")
+  }
+
+  private def resetSentFlags(): Future[Option[Unit]] =
+    mongoLockService.withLock {
+      logger.info(s"Resetting 'sent' flag for all tax checks created on or after $resetTaxChecksCreatedOnOrAfter")
+      taxCheckStore
+        .resetTaxCheckIsExtractedFlag(resetTaxChecksCreatedOnOrAfter)
+        .fold(
+          e => logger.warn("Could not reset 'sent' flags for tax check data", e),
+          _ => logger.info("Successfully reset 'sent' flags for tax check data")
+        )
+    }
+
+}
