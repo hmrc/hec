7c4eab79cd185a2b6e9c297adcb328d4f4de1fe0
Bonnie Parker <45689313+parkerbonnie@users.noreply.github.com>
2025-03-14 14:49:56 +0000

    Revert "DDCYLS-6441: App Dependency Upgrades (#112)"
    This reverts commit d782d29fc1ac2c8677ca553911a9bab112018605.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,15 @@ import sbt.*
 
 object AppDependencies {
   val playVersion      = "-play-30"
-  val bootstrapVersion = "9.11.0"
-  val hmrcMongoVersion = "2.6.0"
+  val bootstrapVersion = "9.10.0"
+  val hmrcMongoVersion = "2.5.0"
   val pekkoVersion = "1.0.3" //version has to match already existing pekko version in play
 
   val compile = Seq(
     "uk.gov.hmrc"             %% s"bootstrap-backend$playVersion"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo$playVersion"           % hmrcMongoVersion,
     "uk.gov.hmrc"             %% s"domain$playVersion"               % "10.0.0",
-    "uk.gov.hmrc"             %% s"internal-auth-client$playVersion" % "3.1.0",
+    "uk.gov.hmrc"             %% s"internal-auth-client$playVersion" % "3.0.0",
     "org.typelevel"           %% "cats-core"                         % "2.13.0",
     "uk.gov.hmrc.objectstore" %% s"object-store-client$playVersion"  % "2.1.0",
     "com.beachape"            %% "enumeratum-play"                   % "1.8.2"
