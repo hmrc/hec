bb5f6932ca9f17c79a4bf5a09ef03033068fcdca
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2025-07-22 15:35:49 +0100

    DDCYLS-6994: Bootstrap 19.8.0 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
   val playVersion      = "-play-30"
-  val bootstrapVersion = "9.17.0"
+  val bootstrapVersion = "9.18.0"
   val hmrcMongoVersion = "2.7.0"
   val pekkoVersion = "1.0.3" //version has to match already existing pekko version in play
 
