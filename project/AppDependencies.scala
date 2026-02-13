6c72ab168fb45b804d2ae59c221f3878a1848838
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-02-13 13:26:50 +0000

    updating dependencies (#123)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,7 +14,10 @@ object AppDependencies {
     "uk.gov.hmrc"             %% s"internal-auth-client$playVersion" % "4.3.0",
     "org.typelevel"           %% "cats-core"                         % "2.13.0",
     "uk.gov.hmrc.objectstore" %% s"object-store-client$playVersion"  % "2.5.0",
-    "com.beachape"            %% "enumeratum-play"                   % "1.9.4"
+    "com.beachape"            %% "enumeratum-play"                   % "1.9.4",
+    "org.apache.commons"      % "commons-lang3"                      % "3.18.0",
+    "ch.qos.logback"          % "logback-core"                       % "1.5.21",
+    "at.yawk.lz4"             %  "lz4-java"                          % "1.10.3"
   )
 
   val test = Seq(
